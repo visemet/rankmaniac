3a6795eb503b4aaeb677e78a5cb91136f6f72f1c
Max Hirschhorn <maxh@caltech.edu>
2014-01-20 04:25:20 -0800

    Added handlers for `run` and `kill` commands.
    Now able to start jobs via specifying the team name, which can later be
    terminated either by specifying the **internal** job identifier, or the
    team identifier.

--- manage.py
+++ manage.py
@@ -10,13 +10,17 @@ Authored by: Max Hirschhorn (maxh@caltech.edu)
 
 from boto.exception import EmrResponseError
 
+from config import S3_GRADING_BUCKET
 from config import teams, datasets
+
+from grader import Grader
 
 assert len(datasets) >= 0
 
 jobs = []
+job_ids_by_team_id = {}
 
-bucket = 'cs144grading'
+bucket = S3_GRADING_BUCKET
 infile = datasets[0]
 
 def handle_list(args):
@@ -32,13 +36,14 @@ def handle_list(args):
     # display the state of each job
     elif args.kind == 'jobs':
         for i, r in enumerate(jobs):
-            while True:
-                try:
-                    state = r.describe().state
-                    print('[%d]  %15s  %s' % (i, state, r.job_id))
-                    break
-                except EmrResponseError:
-                    sleep(10) # call Amazon APIs infrequently
+            if r is not None:
+                while True:
+                    try:
+                        state = r.describe().state
+                        print('[%d]  %15s  %s' % (i, state, r.job_id))
+                        break
+                    except EmrResponseError:
+                        sleep(10) # call Amazon APIs infrequently
 
     # display the filename of all available datasets
     elif args.kind == 'datasets':
@@ -70,6 +75,48 @@ def handle_set(args):
             raise Exception('invalid dataset')
         infile = args.value
 
+def handle_run(args):
+    """
+    Handler for the `run` command.
+    """
+
+    team_id = args.team
+    if team_id not in teams:
+        raise Exception('invalid team')
+
+    if team_id not in job_ids_by_team_id:
+        g = Grader(team_id, infile)
+        if g.do_setup():
+            jobs.append(g)
+
+            job_id = len(jobs) - 1
+            job_ids_by_team_id[team_id] = job_id
+        else:
+            print('No submission to run.')
+
+def handle_kill(args):
+    """
+    Handler for the `kill` command.
+    """
+
+    if args.kind == 'team':
+        team_id = args.team
+        if team_id not in teams or team_id not in job_ids_by_team_id:
+            raise Exception('invalid team')
+        job_id = job_ids_by_team_id[team_id]
+
+    elif args.kind == 'job':
+        job_id = args.job
+        if job_id >= len(jobs):
+            raise Exception('invalid job')
+
+    job = jobs[job_id]
+    if job is None:
+        raise Exception('job already terminated')
+
+    jobs[job_id] = None
+    del job
+
 if __name__ == '__main__':
     import argparse
 
@@ -93,6 +140,24 @@ if __name__ == '__main__':
     parser_set.add_argument('name', choices=['bucket', 'infile'])
     parser_set.add_argument('value')
     parser_set.set_defaults(func=handle_set)
+
+    # Create the parser for the run command
+    parser_run = subparsers.add_parser('run')
+    parser_run.add_argument('team', choices=teams)
+    parser_run.set_defaults(func=handle_run)
+
+    # Create the parser for the kill command
+    parser_kill = subparsers.add_parser('kill')
+    parser_kill.set_defaults(func=handle_kill)
+    subparsers_kill = parser_kill.add_subparsers(dest='kind')
+
+    parser_kill_team = subparsers_kill.add_parser('team')
+    parser_kill_team.add_argument('team', metavar='TEAM', choices=teams,
+                                  help='the team identifier')
+
+    parser_kill_job = subparsers_kill.add_parser('job')
+    parser_kill_job.add_argument('job', metavar='JOB', type=int,
+                                 help='the job identifier')
 
     while True:
         try:
